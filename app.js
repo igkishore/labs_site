24878fd3486c14765a37e248d739dcb20a47987d
igkishore <gowthamkishoreindukuri@gmail.com>
2021-04-07 11:25:13 +0530

    v8

--- app.js
+++ app.js
@@ -65,39 +65,17 @@ const projectroutes = require('./routes/projectroutes');
 const blogroutes = require('./routes/blogroutes');
 const memberroutes = require('./routes/memberroutes.js');
 
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/logout',(req,res) =>{
   console.log('logout')
   req.logout();
   res.redirect('/');
 })
 
-
-
-
 //Central dashboard
 app.get('/centraldashboard',ensureAuthenticated,(req,res)=>{
   console.log('Central Dashboard');
   res.render('centraldashboard',{error:''});
 })
-
-
-
-
-
-
-
 
 //Member Dashboard
 app.get('/memberdashboard',ensureAuthenticated,(req,res) =>{
@@ -287,10 +265,6 @@ app.post('/memberupdate/:id',ensureAuthenticated,(req,res)=>{
           if(req.body.user_image)
           {
             object.user_image = req.body.user_image;
-            /*object.user_image ={
-              data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-              contentType: 'image/png'
-            }*/
           }
           if(req.body.password1 && req.body.password2)
           {
@@ -313,21 +287,6 @@ app.post('/memberupdate/:id',ensureAuthenticated,(req,res)=>{
     })
   }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -511,13 +470,6 @@ app.post('/adminaddmember',ensureAuthenticated, upload.single('image'),(req,res)
    
 })
 
-
-
-
-
-
-
-
 //Admin edit
 
 
@@ -567,8 +519,6 @@ app.delete('/admineditproject/:id',ensureAuthenticated,(req,res)=>{
   .catch(err=>console.log(err));
 
 })
-
-
 
 app.get('/admineditblog',ensureAuthenticated,(req,res) =>{
   const user_id = req.session.passport.user;
@@ -665,38 +615,6 @@ app.delete('/admineditmember/:id',ensureAuthenticated,(req,res)=>{
 
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //review
 
 app.get('/adminreviewproject',ensureAuthenticated,(req,res) =>{
@@ -723,7 +641,6 @@ app.get('/adminreviewproject',ensureAuthenticated,(req,res) =>{
 })
 
 app.get('/adminreviewprojectparticular/:id',ensureAuthenticated,(req,res)=>{
-  //console.log("hhhhhhhhhhhhhhhh")
   const user_id = req.session.passport.user;
   user_db.findById(user_id)
   .then(result => {
@@ -760,7 +677,6 @@ app.get('/adminreviewprojectparticular/:id',ensureAuthenticated,(req,res)=>{
 
 })
 
-
 app.delete('/admindeleteprojectparticular/:id',ensureAuthenticated,(req,res)=>{
   const user_id = req.session.passport.user;
   user_db.findById(user_id)
@@ -784,10 +700,6 @@ app.delete('/admindeleteprojectparticular/:id',ensureAuthenticated,(req,res)=>{
   .catch(err=>console.log(err));
 
 })
-
-
-
-
 
 app.get('/adminreviewblog',ensureAuthenticated,(req,res) =>{
   const user_id = req.session.passport.user;
@@ -837,7 +749,6 @@ app.delete('/admindeleteblogparticular/:id',ensureAuthenticated,(req,res)=>{
 })
 
 
-
 app.get('/adminreviewblogparticular/:id',ensureAuthenticated,(req,res)=>{
   const user_id = req.session.passport.user;
   user_db.findById(user_id)
@@ -876,24 +787,6 @@ app.get('/adminreviewblogparticular/:id',ensureAuthenticated,(req,res)=>{
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Authentication
 app.get('/login',(req,res)=>{
   const error = req.flash().error  || [];
@@ -917,29 +810,6 @@ app.post('/login',(req,res,next) => {
   })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Index page
 
 app.get('/',(req,res)=>{
@@ -959,351 +829,3 @@ app.use('/members',memberroutes);
 app.use((req,res) =>{
   res.status(404).render('404');
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser=require('body-parser');
-const bcrypt = require('bcrypt');
-const flash  =require('connect-flash');
-const session = require('express-session');
-const passport = require('passport'); 
-const fs = require('fs');
-require('./passport')(passport);
-
-//File Requires
-
-const { ensureAuthenticated } = require('./auth');
-const blogRoutes = require('./routes/blogsroutes');
-const projectRoutes = require('./routes/projectsroutes');
-const userRoutes = require('./routes/memberroutes');
-
-//Database Instances
-const project_db = require('./models/project.model');
-const blog_db = require('./models/blog.model');
-const user_db = require('./models/user.model');
-
-
-//Database Connections and Express
-var app = express();
-const port = process.env.PORT || 3000 ;
-const dburl = "mongodb+srv://gowtham:test1234@main.l0g6f.mongodb.net/dataPirates_db?retryWrites=true&w=majority";
-mongoose.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(res=> app.listen(port),console.log("Database  Connected !!!"))
-.catch(err=> console.log(err));
-
-//View Engine
-
-
-//Static and MiddleWare
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.json());
-app.use(session({
-  secret: 'gowtham',
-  resave: true,
-  saveUninitialized: true,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-
-//Multer
-var path = require('path');
-var multer = require('multer');
-
-var storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'uploads')
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.fieldname + '-' + Date.now())
-	}
-});
-
-var upload = multer({ storage: storage });
-
-
-
-//Main Route
-app.get('/',(req,res)=>{
-    res.render('aaindex');
-    console.log('Rendered Index');
-})
-
-//Project Route
-app.use('/projects',projectRoutes);
-/*
-//Memeber Route
-app.use('/members',memberRoutes);
-
-//Blog Route
-app.use('/blogs',blogRoutes);
-
-//404
-app.use((req, res) => {
-  res.status(404).render('404');
-});
-
-
-
-
-//Admin
-//app.use('/admin',adminRoutes);
-
-//Member
-//app.use('/member',MemberRoutes);
-
-
-//404 Page
-
-
-/*
-//admin
-app.get('/adminlogin',(req,res)=>{
-    res.render('adminlogin');
-    console.log('adminRendered Login');
-})
-
-app.get('/adminregister',(req,res)=>{
-    res.render('adminregister');
-    console.log('Rendered adminRegister');
-})
-
-app.get('/admindashboard',(req,res)=>{
-  res.render('admindashboard');
-  console.log('Rendered admindashboard');
-})
-
-app.get('/editprojects',(req,res)=>{
-  res.render('editprojects');
-  console.log('Rendered Edit project')
-})
-
-
-
-app.get('/editmembers',(req,res)=>{
-  res.render('editmembers');
-  console.log('Rendered Edit members')
-})
-
-app.post('/al',(req,res,next)=>{
-  passport.authenticate('local',{
-    successRedirect:'admindashboard',
-    failureRedirect:'adminlogin',
-    failureFlash:true
-  }) (req,res,next);
-})
-
-app.post('/ar',(req,res)=>{
-  const {name,password} = req.body;
-  const ps = new apdb({name,password});
-  bcrypt.genSalt(10,(err,salt) =>
-      bcrypt.hash(ps.password,salt,(err,hash) =>
-      {
-        if(err) throw err;
-        ps.password =hash;
-        ps.save()
-        .then(user =>
-          {
-            res.redirect('/adminlogin')
-          })
-        .catch(err => console.log(err));
-      })
-    )
-})
-
-app.get('/logout',(req,res) =>{
-  req.logout();
-  res.redirect('adminlogin');
-})
-
-
-//projects
-app.get('/projects',(req,res) =>{
-    pjdb.find().sort({"created_at":  -1})
-    .then(result =>{
-      console.log(result)
-      res.render('projects',{pj:result});
-      console.log('projects rendered');
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
-
-app.get('/newproject',(req,res) =>{
- res.render('newproject');
- console.log('new project rendered')
-})
-
-app.post('/np', upload.single('image'), (req, res, next) => {
- console.log(req.body);
-var obj = {
-		title: req.body.title,
-      point1: req.body.point1,
-      point2: req.body.point2,
-      link1: req.body.link1,
-      link2: req.body.link2,
-      introduction : req.body.introduction,
-      working : req.body.working,
-      conlcusion : req.body.conlcusion,
-		  image: {
-        data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-        contentType: 'image/png'
-		  }
-	}
-  
-	pjdb.create(obj, (err, item) => {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			res.redirect('/projects');
-		}
-	});
-});
-
-
-
-
-
-
-
-app.get('/projectdetail/:id',(req,res)=>{
-  const id = req.params.id;
-  pjdb.findById(id)
-  .then(result =>{
-   res.render('projectdetails',{pj : result});
-  })
-  .catch(err => {
-    console.log(err);
-  })
-});
-
-
-
-
-//members
-app.get('/members',(req,res) =>{
-  mbdb.find().sort({"created_at":  -1})
-  .then(result =>{
-    res.render('members',{mb:result});
-    console.log('members rendered');
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
-
-app.get('/newmember',(req,res) =>{
-res.render('newmember');
-console.log('new member rendered')
-});
-
-app.post('/nm', (req, res) => {
-const nmb = new mbdb(req.body);
-nmb.save()
-.then(result=> {
-  res.redirect('members');
-})
-.catch(err=>{
-  console.log(err);
-})
-});
-
-
-app.get('/members/:id',(req,res)=>{
-  const id = req.params.id;
-  mbdb.findById(id)
-  .then(result =>{
-   res.render('memberdetails',{mb : result});
-  })
-  .catch(err => {
-    console.log(err);
-  })
-});
-
-
-
-
-
-
-
-
-//blogs 
-app.get('/blogs',(req,res) =>{
-    bgdb.find().sort({createdAt: -1})
-    .then(result =>{
-      res.render('blogs',{bg:result});
-    })
-    .catch(err => {
-      console.log(err);
-    })
-});
-
-app.get('/newblog',(req,res)=>
-{
-    res.render('newblog');
-    console.log('new blog Rendered');
-});
-
-app.post('/nb',(req,res)=>
-{
-   const bg = new bgdb(req.body);
-   bg.save()
-   .then(result=>{
-       res.redirect('blogs');
-   })
-   .catch(err=>{
-       console.log(err);
-   })
-})
-
-app.get('/blogdetail/:id',(req,res) =>
-{
-  const id = req.params.id;
-  bgdb.findById(id)
-  .then(result =>{
-   res.render('blogdetails',{bgs : result});
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
-
-app.get('/editblogs',(req,res) =>{
-  bgdb.find().sort({createdAt: -1})
-  .then(result =>{
-    res.render('editblogs',{bgs:result});
-  })
-  .catch(err => {
-    console.log(err);
-  })
-});*/
